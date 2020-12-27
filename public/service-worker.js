e893c57135defc810087ca658f6a5243cca6ace9
Chloe Blackwell <chloeblackwell2@hotmail.com>
2020-12-27 13:31:41 +1100

    changes made

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,42 @@
-const CACHE_NAME = 'static-cache-bt-v1';
-const DATA_CACHE_NAME = 'data-cache-bt-v1';
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+    (size) => `/icons/icon-${size}x${size}.png`
+);
 
 const staticFilesToPreCache = [
-    '/',
-    '/index.js',
-    '/index.html',
-    '/db.js',
-    '/favicon.ico',
-    '/manifest.json',
-    '/style.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
-];
+    "/",
+    "/index.js",
+    "/chart.js",
+    "/manifest.webmanifest",
+    "/db.js",
+    "/index.html",
+    "/styles.css",
+].concat(iconFiles);
 
 
-// Install
-self.addEventListener("install", (evt) => {
+// install
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(staticFilesToPreCache);
         })
     );
 
     self.skipWaiting();
 });
 
-// Activate
-self.addEventListener("activate", (evt) => {
-
+// activate
+self.addEventListener("activate", function (evt) {
     evt.waitUntil(
-        caches.keys().then((keyList) => {
+        caches.keys().then(keyList => {
             return Promise.all(
-                keyList.map((key) => {
+                keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
                         return caches.delete(key);
                     }
                 })
@@ -43,36 +47,45 @@ self.addEventListener("activate", (evt) => {
     self.clients.claim();
 });
 
-// Fetch
-self.addEventListener("fetch", (evt) => {
-
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+// fetch
+self.addEventListener("fetch", function (evt) {
+    const { url } = evt.request;
+    if (url.includes("/api")) {
         evt.respondWith(
-            caches
-                .open(DATA_CACHE_NAME)
-                .then((cache) => {
-                    return fetch(evt.request)
-                        .then((response) => {
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request, response.clone());
+                        }
 
-                            if (response.status === 200) {
-                                cache.put(evt.request, response.clone());
-                            }
-
-                            return response;
-                        })
-                        .catch(() => {
-
-                            return cache.match(evt.request);
-                        });
-                })
-                .catch((err) => console.log(err))
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
         );
+    } else {
 
-        return;
+        evt.respondWith(
+
+            caches.match(evt.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches
+                    .open(DATA_CACHE_NAME)
+                    .then(cache =>
+                        fetch(evt.request).then(response =>
+                            cache.put(evt.request, response.clone()).then(() => response)
+                        )
+                    );
+            })
+
+        );
     }
-    evt.respondWith(
-        caches.match(evt.request).then((response) => {
-            return response || fetch(evt.request);
-        })
-    );
 });
